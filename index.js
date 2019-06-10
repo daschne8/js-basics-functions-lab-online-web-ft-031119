9ccaf37cb95dc94e542c2a85591dfe0278f9590c
Daniel Schneider <daschne8@gmail.com>
2019-06-10 19:39:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ function distanceFromHqInFeet(block){
 function distanceTravelledInFeet(starting, ending){
   return Math.abs(starting - ending) * 264
 }
-function calculatesFarePrice(block){
+function calculatesFarePrice(starting,ending){
+  let travelled =  distanceTravelledInFeet(starting,ending)
+  let response
+  if (travelled <= 400) {
+    response = 0
+  }
+  else if (400 < travelled <= 2000) {
+    response = travelled * .02
+  }
+  else if (2000 < travelled <= 2500) {
+    response = 25
+  }
+  else if (travelled > 2500) {
+    response = 'cannot travel that far'
+  }
 
+  return response
 }
